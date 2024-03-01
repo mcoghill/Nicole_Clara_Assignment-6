f60f0444c1467c90ddf794b228ad0fccedb4c3b9
ClaraBB1 <159877596+ClaraBB1@users.noreply.github.com>
2024-03-01 14:20:04 -0800

    Update Assign6Main.R
    extra script (practice from Nicole?)

--- Assign6Main.R
+++ Assign6Main.R
@@ -1,17 +1,1 @@
-install.packages("tidyverse")
-install.packages("readxl")
-install.packages("sf")
-install.packages("terra")
-install.packages("mapview")
-install.packages("bcdata")
-install.packages("bcmaps")
 
-library(tidyverse)
-library(readxl)
-library(sf)
-library(terra)
-library(mapview)
-library(bcdata)
-library(bcmaps)
-
-##
