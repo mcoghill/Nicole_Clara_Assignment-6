84994d53631a260597925b88901561e4bbf7d067
plicata13 <159927290+plicata13@users.noreply.github.com>
2024-03-04 18:57:12 -0800

    Update PartA_Fire.R
    Update PartA_Fire.R to include packages and libraries

--- PartA_Fire.R
+++ PartA_Fire.R
@@ -1,3 +1,19 @@
+install.packages("tidyverse")
+install.packages("readxl")
+install.packages("sf")
+install.packages("terra")
+install.packages("mapview")
+install.packages("bcdata")
+install.packages("bcmaps")
+
+library(tidyverse)
+library(readxl)
+library(sf)
+library(terra)
+library(mapview)
+library(bcdata)
+library(bcmaps)
+
 
 fire_search <- bcdc_search("fire incident historical")
 View(fire_search)
