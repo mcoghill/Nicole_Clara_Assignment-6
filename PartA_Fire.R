1cfe71a8b8f31c1d3c94177922e6071c11b5eb69
plicata13 <159927290+plicata13@users.noreply.github.com>
2024-03-05 16:27:32 -0800

    Update PartA_Fire.R
    Clipping AOI method edited, not BBOX

--- PartA_Fire.R
+++ PartA_Fire.R
@@ -15,25 +15,36 @@ library(bcdata)
 library(bcmaps)
 
 
-fire_search <- bcdc_search("fire incident historical")
-View(fire_search)
-
-
-fire_incidents <- bcdc_query_geodata("e2dadc60-292f-4d98-b42b-56ca9e4fe694")
-
-bcdc_describe_feature("https://catalogue.data.gov.bc.ca/dataset/e2dadc60-292f-4d98-b42b-56ca9e4fe694")
-
-
 ldb <- bcdc_query_geodata("1130248f-f1a3-4956-8b2e-38d29d3e4af7", crs = 3005) |> 
   filter(PROTECTED_LANDS_NAME == "LAC DU BOIS GRASSLANDS PROTECTED AREA") |> 
   collect()
 
+fire_search <- bcdc_search("fire incident historical")
+View(fire_search)
+
+bcdc_describe_feature("https://catalogue.data.gov.bc.ca/dataset/e2dadc60-292f-4d98-b42b-56ca9e4fe694")
+
+####option 1
+fire_incidents <- bcdc_query_geodata("e2dadc60-292f-4d98-b42b-56ca9e4fe694")
 
 fire_incidents_ldb <- fire_incidents %>%
-  filter(BBOX(ldb)) %>%
+  filter(INTERSECTS(ldb)) %>%
   collect()
 
 mapview(fire_incidents_ldb)
+
+##or option 2 - both have 279 observations/points with 21 variables
+
+fire_search <- bcdc_search("fire incident locations")
+View(fire_search)
+
+fires_aoi <- bcdc_query_geodata(fire_search[[1]]$id, crs = 3005) %>% 
+  filter(INTERSECTS(ldb)) %>% 
+  collect()
+
+mapview(fires_aoi)
+
+
 
 fires_summary <- fire_incidents_ldb %>%
   group_by(FIRE_YEAR) %>%
@@ -43,11 +54,10 @@ cause_summary <- fire_incidents_ldb %>%
   group_by(FIRE_CAUSE) %>%
   summarize(fires_count = n())
 
-
 ##FIRE_CAUSE   fires_count
-##Lightning    123
-##Person       352
-##Unknown      290
+##Lightning    63
+##Person       118
+##Unknown      98
 
 mapview(fire_incidents_ldb, zcol = "FIRE_CAUSE")
 
@@ -66,7 +76,7 @@ ggplot(mean_fires_by_cause, aes(x = FIRE_CAUSE, y = mean_fires_per_year, fill = 
   labs(x = "Fire Cause", y = "Mean Fires per Year") +
   theme_minimal()
 
-
+##a bar chart may display the data better:
 ggplot(cause_summary, aes(x = FIRE_CAUSE, y = fires_count, fill = FIRE_CAUSE)) +
   geom_bar(stat = "identity") +
   labs(x = "Fire Cause", y = "Fires Count") +
